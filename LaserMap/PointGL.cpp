e2eddf7ed21c7ad272fae9a309987d274ec0cc12
italogar6 <italogar6@hotmail.com>
2016-03-03 11:37:38 +0000

    rectangulo con poligono 0303

--- LaserMap/PointGL.cpp
+++ LaserMap/PointGL.cpp
@@ -23,18 +23,23 @@ void PointGL::paintEvent(QPaintEvent *e)
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glRotatef(spin, 0.0, 0.0, 1.0);
-	glColor3f(0.0, 0.5, 1.0);
+	glColor3f(1.0, 1.0, 1.0);
 	glRectf(-25.0, -25.0, 25.0, 25.0);
 
+	/*
 	spin = spin + 2.0;
 	if (spin > 360.0)
 		spin = spin - 360.0;
-
-	/*
-	glBegin(GL_POINTS);
-	glVertex3f(25.0, 25.0, 0.0);
-	glEnd();
 	*/
+	glBegin(GL_POLYGON);
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(20.0, 20.0, 0.0);
+	glVertex3f(25.0, 0.0, 0.0);
+	glVertex3f(20.0, -20.0, 0.0);
+	glVertex3f(-20.0, -20.0, 0.0);
+	glVertex3f(-20.0, 20.0, 0.0);
+	glEnd();
+	/**/
 }
 
 void PointGL::resizeGL(int w, int h)
